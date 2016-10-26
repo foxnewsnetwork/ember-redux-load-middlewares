c7b358050eb7cd71b261508da3436c2a8a0d2afd
Thomas Chen <tom.chen@am.sony.com>
2016-10-26 10:36:12 -0700

    added reducer blueprint

---
+++ blueprints/redux-reducer/files/__root__/reducers/__name__.js
@@ -0,0 +1,8 @@
+export function reducer(state, action) {
+  return state;
+}
+
+export default {
+  name: '<%= dasherizedModuleName %>',
+  reducer
+};
---
+++ tests/dummy/app/reducers/applesauce.js
@@ -0,0 +1,8 @@
+export function reducer(state, action) {
+  return state;
+}
+
+export default {
+  name: 'applesauce',
+  reducer
+};
